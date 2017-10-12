bc1a45caf76f1c9c5510db965fabcb89a4621d3b
errornosignal <errornosignal@users.noreply.github.com>
2017-10-12 11:49:47 -0500

    end of class 10/12/17

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
 
     public static void main(String []args) {
 
+        Tree234<String> tree = new Tree234<>();
     }
 
 }
--- src/Tree234.java
+++ src/Tree234.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class Tree234<T extends Comparable<? super T>> implements Tree<T> {
 
@@ -18,11 +20,11 @@ public class Tree234<T extends Comparable<? super T>> implements Tree<T> {
         private static final int ORDER = 4;
         private int numberOfItems;
         private Node parent;
-        private Node childArray[];
-        private DataItem itemArray[];
+        private List<Node> childArray;
+        private List<DataItem> itemArray;
 
         public Node getChild(int index) {
-            return this.childArray[index];
+            return this.childArray.get(index);
         }
 
         public Node getParent() {
@@ -34,7 +36,7 @@ public class Tree234<T extends Comparable<? super T>> implements Tree<T> {
         }
 
         public DataItem getItem (int index) {
-            return this.itemArray[index];
+            return this.itemArray.get(index);
         }
 
         public boolean isFull() {
@@ -42,7 +44,7 @@ public class Tree234<T extends Comparable<? super T>> implements Tree<T> {
         }
 
         public boolean isLeaf() {
-            return this.childArray[0] == null;
+            return this.childArray.get(0) == null;
         }
 
         //You must explain in detail why this is a good idea
@@ -50,17 +52,19 @@ public class Tree234<T extends Comparable<? super T>> implements Tree<T> {
         public Node() {
             this.numberOfItems = 0;
             this.parent = null;
-            this.childArray = (Node[]) new Object[ORDER];
-            this.itemArray = (DataItem[]) new Object[ORDER -1];
+            this.childArray = new ArrayList<>(ORDER);
+            this.itemArray = new ArrayList<>(ORDER - 1);
+
+            for (int index = 0; index < ORDER; index++);
         }
 
         public int findItem(T key) {
             int index = 0;
             while (index < ORDER -1) {
-                if (this.itemArray[index] == null) {
+                if (this.itemArray.get(index) == null) {
                     return - 1;
                 }
-                else if (this.itemArray[index].data.compareTo(key) == 0) {
+                else if (this.itemArray.get(index).data.compareTo(key) == 0) {
                     return index;
                 }//else not found doNothing();
                 index++;
@@ -70,8 +74,8 @@ public class Tree234<T extends Comparable<? super T>> implements Tree<T> {
 
         //removes highest value item (value at the end)
         public DataItem removeItem() {
-            DataItem temp = this.itemArray[this.numberOfItems - 1];
-            this.itemArray[this.numberOfItems - 1] = null;
+            DataItem temp = this.itemArray.get(this.numberOfItems - 1);
+            this.itemArray.set(this.numberOfItems - 1, null);
             this.numberOfItems--;
             return temp;
         }
@@ -80,30 +84,75 @@ public class Tree234<T extends Comparable<? super T>> implements Tree<T> {
         public int insertItem(DataItem newItem) {
             T newKey = newItem.data;
             for(int index = ORDER - 2; index >= 0; index--) {
-                if (this.itemArray[index] != null) {
-                    T currentKey = this.itemArray[index].data;
+                if (this.itemArray.get(index) != null) {
+                    T currentKey = this.itemArray.get(index).data;
                     if (newKey.compareTo(currentKey) < 0) {  //currentKey larger than newKey
-                        this.itemArray[index - 1] = this.itemArray[index];
+                        this.itemArray.set(index + 1, this.itemArray.get(index));
                     }
                     else { //currentKey is smaller that newKey
-                        this.itemArray[index + 1] = this.itemArray[index];
+                        this.itemArray.set(index + 1, this.itemArray.get(index));
                         return index + 1;
                     }
                 }
             }
-            this.itemArray[0] = newItem;
+            this.itemArray.set(0, newItem);
             return 0;
         }
     }
 
+    private Node root = new Node();
+
     @Override
     public boolean add (T element){
-        return false;
+        Node current = this.root;
+        DataItem tempItem = new DataItem(element);
+        boolean hasHitLeaf = false;
+        while(!hasHitLeaf) {
+            if (current.isFull()) {
+                this.split(current);
+                //back it up and search again
+                current = current.getParent();
+                current = this.getNextChild(current, element);
+            }
+            else if (current.isLeaf()) {
+                hasHitLeaf = true;
+            }
+            else {
+                current = this.getNextChild(current, element);
+            }
+        }
+        current.insertItem(tempItem);
+        return true;
     }
 
     @Override
     public boolean contains (T element){
-        return false;
+        Node current = this.root;
+        int childNumber = 0;
+        while(true) {
+            if ((childNumber = current.findItem(element)) >= 0) {
+                    return true;
+            }
+            else if (current.isLeaf()) {
+                return false;
+            }
+            else {
+                current = this.getNextChild(current, element);
+            }
+        }
+    }
+
+    private Node getNextChild(Node current, T element) {
+        int numberOfItems = current.getNumberOfItems();
+        int index = 0;
+        while (index < numberOfItems) {
+            // is element (key) less than the value of the data at the current index?
+            if (element.compareTo(current.getItem(index).data) < 0) {
+                return current.getChild(index);
+            } // else element is greater than data, doNothing();
+            index++;
+
+        return current.getChild(index);
     }
 
     @Override
